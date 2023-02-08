d67ed62a1507d644ddefeed7b795d01dcbcfa186
Steffen Neumann <sneumann@ipb-halle.de>
2023-02-08 13:55:34 +0100

    Add a README, dockerize the app

---
+++ install.R
@@ -0,0 +1,9 @@
+options(repos = c(CRAN = "http://cran.rstudio.com"))
+
+## dependencies:
+install.packages("ggplot2")
+install.packages("cowplot")
+install.packages("DT")
+install.packages("dplyr")
+install.packages("BiocManager")
+install.packages("devtools")
