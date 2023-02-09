973903c826bd7012da0b8cec9aac6210033463f2
RogerGinBer <45530914+RogerGinBer@users.noreply.github.com>
2023-02-09 12:00:23 +0100

    refactor: abstract margin plots

--- utils.R
+++ utils.R
@@ -16,8 +16,8 @@ calculate_4d_matrix_from_Spectra <- function(Spectra){
                              pd = pd,
                              rt = spectraData(Spectra, c("rtime")),
                              im = spectraData(Spectra, c("inv_ion_mobility"))
+                             )
                       )
-        )
         colnames(sp) <- c("mz", "intensity", "retention_time", "inv_ion_mobility")
     }
     sp
@@ -28,19 +28,34 @@ create_marginal_plot <- function(data){
     main_immz <- ggplot(data) + geom_point(aes(x=inv_ion_mobility, y=mz, color = log10(intensity)))  + theme_minimal() +theme(legend.position = "none") 
     
     
-    margin_rt <- do.call(cbind, summarize(group_by(data, retention_time), intensity = sum(intensity)))  
-    margin_rt_plot <- ggplot(as.data.frame(margin_rt)) + geom_line(aes(x=retention_time, y=intensity)) + theme_minimal()
-    
-    margin_im <- do.call(cbind, summarize(group_by(data, inv_ion_mobility), intensity = sum(intensity))) 
-    margin_im_plot <- ggplot(as.data.frame(margin_im)) + geom_line(aes(x=inv_ion_mobility, y=intensity)) + theme_minimal()
-    
-    margin_mz <- do.call(cbind, summarize(group_by(data, mz), intensity = sum(intensity))) 
-    margin_mz_plot <- ggplot(as.data.frame(margin_mz)) + geom_point(aes(x=mz, y=intensity)) + coord_flip() + theme_minimal()
-    
+    if ("dataOrigin" %in% colnames(data)) {
+        margin_rt_plot <- calculate_margin_plot_from_data(data, "retention_time", bySample = TRUE)
+        margin_im_plot <- calculate_margin_plot_from_data(data, "inv_ion_mobility", bySample = TRUE)
+        margin_mz_plot <- calculate_margin_plot_from_data(data, "mz", bySample = TRUE) + coord_flip()
+    } else {
+        margin_rt_plot <- calculate_margin_plot_from_data(data, "retention_time")
+        margin_im_plot <- calculate_margin_plot_from_data(data, "inv_ion_mobility")
+        margin_mz_plot <- calculate_margin_plot_from_data(data, "mz") + coord_flip()
+    }
+
     plot <- cowplot::plot_grid(margin_im_plot, margin_rt_plot, NULL,
                                main_immz,      main_rtmz,      margin_mz_plot,
                                rel_heights = c(0.3, 0.7),
                                rel_widths = c(0.4, 0.4, 0.2),
                                nrow = 2, ncol = 3)
     return(plot)
+}
+
+calculate_margin_plot_from_data <- function(data, groupVar, bySample = FALSE, sampleVar = "dataOrigin"){
+    if(bySample) groupVar <- c(groupVar, sampleVar)
+    margin_data <- do.call(cbind, summarize(group_by(data, !!sym(groupVar)), intensity = sum(intensity)))
+    if(bySample){
+        return(ggplot(as.data.frame(margin_data)) +
+            geom_line(aes(x=groupVar, y=intensity, color = .data[[sampleVar]], group = .data[[sampleVar]])) +
+            theme_minimal())
+    } else {
+        return(ggplot(as.data.frame(margin_data)) +
+            geom_line(aes(x=.data[[groupVar]], y=intensity)) +
+            theme_minimal())
+    }
 }
