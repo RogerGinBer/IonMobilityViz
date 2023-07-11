0a48c29da73abd79879cd8dd2746d7a2cc70b7e8
RogerGinBer <45530914+RogerGinBer@users.noreply.github.com>
2023-07-11 11:18:56 +0200

    Add force=TRUE to install_github
    Ensure all deps are installed during Docker build

--- install2.R
+++ install2.R
@@ -1,6 +1,6 @@
 ## Special dependencies that might change more often
 ## Bruker MS data interface
 BiocManager::install('Spectra')
-remotes::install_github("michalsta/opentims", subdir="opentimsr")
-remotes::install_github('rformassspectrometry/MsBackendTimsTof')
-remotes::install_github(repo="RogerGinBer/xcms", ref="spectra-ion-mobility")
+remotes::install_github("michalsta/opentims", subdir="opentimsr", force = TRUE)
+remotes::install_github('rformassspectrometry/MsBackendTimsTof', force = TRUE)
+remotes::install_github(repo="RogerGinBer/xcms", ref="spectra-ion-mobility", force = TRUE)
